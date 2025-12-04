c9b1d822f7c772eca46906c726e8d95945b17b9f
Tokke2553 <linjunjie13579@gmail.com>
2025-12-04 16:30:04 +0100

    Tipo de arma

--- Practica_POO/src/Paquete/Arma.java
+++ Practica_POO/src/Paquete/Arma.java
@@ -1,5 +1,22 @@
 package Paquete;
 
 public class Arma {
-
+	private String name;
+	private int damage;
+	
+	public Arma(String name, int damage) {
+		this.name= name;
+		this.damage = damage;
+		
+		
+	}
+	
+	public String getName() {
+		return name;
+		
+	}
+	public int getDamage() {
+		return damage;
+		
+	}
 }
