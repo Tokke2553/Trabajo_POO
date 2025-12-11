be798f4dd953a81ec6ae40022de2108604c215a0
ivanover17 <ivinj55@gmail.com>
2025-12-11 16:40:22 +0100

    ee

--- Practica_POO/src/Paquete/Main.java
+++ Practica_POO/src/Paquete/Main.java
@@ -1,28 +1,9 @@
 package Paquete;
 
-<<<<<<< HEAD
-=======
 
-/*
-import Armas.*;
-import Objetos.*;
-import Armas.Arma;
-*/
-
-import Armas.*;
-import Objetos.*;
-import Armas.Arma;
-
-
-
->>>>>>> ffbca833f5bde964ea9c3ddf510f8eed59310296
 public class Main {
     public static void main(String[] args) {
         JuegoInterfaz juego = new JuegoInterfaz();
         juego.mostrar();
     }
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> ffbca833f5bde964ea9c3ddf510f8eed59310296
