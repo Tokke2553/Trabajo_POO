c398302cf42c1092fcc83b903ed780fe05ae39bc
Gatofinchu <daviddelamanogil2006@gmail.com>
2025-12-11 16:19:39 +0100

    Objetos1

--- Practica_POO/src/Objetos/Objetos.java
+++ Practica_POO/src/Objetos/Objetos.java
@@ -2,5 +2,43 @@ package Objetos;
 
 public class Objetos {
 	
+	private float vida;
+	private float escudo;
+	private float mejora_daño;
+	private float mejora_precision;
+	private float mejora_cargador;
+	private float mejora_alcance;
+	
+	public Objetos(float vida, float escudo, float mejora_daño, float mejora_precision, float mejora_cargador, float mejora_alcance) {
+		this.vida = vida;
+		this.escudo = escudo;
+		this.mejora_daño = mejora_daño;
+		this.mejora_precision = mejora_precision;
+		this.mejora_cargador = mejora_cargador;
+		this.mejora_alcance= mejora_alcance;
+		}
 
+	public float getVida() {
+		return vida;
+	}
+
+	public float getEscudo() {
+		return escudo;
+	}
+
+	public float getMejora_daño() {
+		return mejora_daño;
+	}
+
+	public float getMejora_precision() {
+		return mejora_precision;
+	}
+
+	public float getMejora_cargador() {
+		return mejora_cargador;
+	}
+
+	public float getMejora_alcance() {
+		return mejora_alcance;
+	}	
 }
