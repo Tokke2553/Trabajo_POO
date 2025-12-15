60c6965c2819ea50bbb2cbf117d3b3596e53b4de
Gatofinchu <daviddelamanogil2006@gmail.com>
2025-12-15 13:02:05 +0100

    f

--- Practica_POO/src/Paquete/TestMap.java
+++
@@ -1,9 +0,0 @@
-package Paquete;
-import Tormenta.*;
-public class TestMap {
-	 public static void main(String[] args) {
-	        ArrayMapa mapa = new ArrayMapa();
-	        mapa.ArrayInicializador();
-	        new MapaGUI(mapa);
-	    }
-}
