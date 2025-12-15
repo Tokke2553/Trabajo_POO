04ae78cb70fc5db3cc72d0d871872965f1d23822
Gatofinchu <daviddelamanogil2006@gmail.com>
2025-12-15 22:36:54 +0100

    Delete Practica_POO directory

--- Practica_POO/src/Objetos/Interfaz_Objetos.java
+++
@@ -1,6 +0,0 @@
-package Objetos;
-import Paquete.Jugador;
-public interface Interfaz_Objetos {
-	void usar(Jugador Jugador);
-
-}
--- Practica_POO/src/module-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * 
- */
-/**
- * 
- */
-module Practica_POO {
-	requires java.desktop;//Se pone esto para que funcione el JFRAME y sus imports
-}
